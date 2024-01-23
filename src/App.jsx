7c095c6d79aa28cfcd144a8c1c6cab89b38ed8e9
Johan Swan <jswannieman@gmail.com>
2024-01-23 20:31:54 +1300

    Update App JSX Component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
     <>
       <div className="h-screen bg-[#171999] select-none">
         <div className="w-full max-w-7xl mx-auto shadow-lg shadow-white rounded-md px-12 py-16 text-white">
-          <h1 className="text-2xl text-center text-white mb-8">To Do List</h1>
+          <h1 className="text-2xl text-center text-white mb-8">
+            My To Do List
+          </h1>
           <AddTodo />
           <ListTodos />
         </div>
