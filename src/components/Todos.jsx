4daed210918817d313faba971e3a681f758f937b
Johan Swan <jswannieman@gmail.com>
2024-01-25 15:35:46 +1300

    Update Todos JSX Component

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { deleteTodo } from "../features/todo/todoSlice";
+import { updateTodo, deleteTodo } from "../features/todo/todoSlice";
 
 function Todos({ todo }) {
+  const [todoText, setTodoText] = useState(todo.text);
   const dispatch = useDispatch();
 
   return (
     <div className="flex border border-black/10 rounded-lg bg-[#5c64d2] px-2 py-1 gap-x-3 shadow-sm shadow-white/50 duration-300  text-white">
       <input
         type="text"
-        value={todo.text}
+        value={todoText}
+        onChange={(event) => setTodoText(event.target.value)}
         className="w-full bg-transparent rounded-lg"
-        readOnly
       />
+      <button
+        className="bg-[#14195d] px-2 py-1 border-white border-2"
+        onClick={() => dispatch(updateTodo([todo.id, todoText]))}
+      >
+        Update
+      </button>
       <button
         className="bg-[#14195d] px-2 py-1 border-white border-2"
         onClick={() => dispatch(deleteTodo(todo.id))}
