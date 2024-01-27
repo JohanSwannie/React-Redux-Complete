deecbe7386768d0762c4a6b832b4ddd84e523654
Johan Swan <jswannieman@gmail.com>
2024-01-27 17:13:33 +1300

    Update main JSX Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store/store.js";
+import { PersistGate } from "redux-persist/integration/react"; // Import PersistGate
+import { store, persistor } from "./store/store.js";
 import App from "./App.jsx";
 import "./index.css";
 
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
